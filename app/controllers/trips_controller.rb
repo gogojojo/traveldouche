7a078835b1f04da1c47251af4a58f93de208d801
gogojojo <joseph.gochua@gmail.com>
2015-04-03 19:07:31 -0700

    passing params through form

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,14 @@ class TripsController < ApplicationController
   def index
   end
   def new
+    @search = Trip.new
   end
   def create
+    @search = Trip.new(params.require(:trip).permit(:place))
+    redirect_to search_path(:place => @search.place)
   end 
+  def search
+    @place = params[:place]
+    
+  end
 end
